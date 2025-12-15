ab5dd476f2ad33b395aa001889d99eface9842a4
Thomas Bosch <bosch-thomas@protonmail.com>
2025-12-15 13:27:02 +0100

    Ensure encryption key is set when creating configuration file

--- docker/init.sh
+++ docker/init.sh
@@ -3,12 +3,16 @@
 set -e
 
 APP_PATH="${FASTAPI_CONFIG_PATH:-app.conf}"
+APP_CONFIG_TEMPLATE_PATH="${APP_CONFIG_TEMPLATE_PATH:-app.conf.example}"
 
 echo "➡️ Creating the configuration file"
 if [ -e "$APP_PATH" ]; then
     echo "⚠️ Configuration file already exists. Skipping."
 else
-    cp app.conf.example $APP_PATH
+    cp "$APP_CONFIG_TEMPLATE_PATH" "$APP_PATH"
+
+    ENCRYPTION_KEY="$(openssl rand -base64 32)"
+    sed -i "s|^\(encryption_key=\).*|\1$ENCRYPTION_KEY|" "$APP_PATH"
 fi
 
 echo "Start main process"
